75c100d5e2ce617111d3dbd163663d581127c758
Clinton Mekwunye <mekwunyeclinton22@gmail.com>
2023-01-02 08:17:38 +0100

    adding second week 2 lecture file

---
+++ first course/week2/trunc.go
@@ -0,0 +1,22 @@
+/* Assingment 1
+Write a program which prompts the user to enter a floating point number
+and prints the integer which is a truncated version of the floating point
+number that was entered. Truncation is the process of removing the digits
+to the right of the decimal place.
+*/
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Please enter a floating point number")
+	var floatNum float32
+
+	fmt.Scan(&floatNum)
+
+	intNum := int(floatNum)
+
+	fmt.Println(intNum)
+
+}
